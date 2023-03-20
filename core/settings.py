a7125a07f806d35b4af0b09f47556df0656a236d
oleksii_svintsitskyi <nananaboston@gmail.com>
2023-03-20 18:20:13 +0800

    Add dir variables and other settings improvements

--- core/settings.py
+++ core/settings.py
@@ -1,10 +1,17 @@
+import os
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     # Lets say, for the ease of this example, that we have a
     # static token that we use for authentication
-    AUTH_TOKEN: str
+    auth_token: str
+    data_dir: str = "data"
+    file_chunk_size: int = 1024 * 1024  # 1 MB
+    max_file_size: int = 1024 * 1024 * 5  # 25 MB
+    max_filename_length: int = 255
+    root_dir: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 settings = Settings()
